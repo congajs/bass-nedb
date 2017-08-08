e48ca2fe716eb7256c35d7a1a79d997e463e448f
Anthony Matarazzo <createvibe@gmail.com>
2017-08-08 02:19:58 -0700

    add adapter path to integration test

--- spec/IntegrationSpec.js
+++ spec/IntegrationSpec.js
@@ -1,3 +1,9 @@
+const path = require('path');
+
 const AdapterIntegrationSpec = require('../node_modules/bass/spec/AdapterIntegrationSpec');
 
-describe('bass-nedb', AdapterIntegrationSpec('bass-nedb'));
+describe('bass-nedb', AdapterIntegrationSpec('bass-nedb', {
+    adapters: [
+        path.resolve('')
+    ]
+}));
