47a398c949cd57be7bc6862abcc5891406c1369a
Anthony Matarazzo <createvibe@gmail.com>
2017-08-08 14:11:48 -0700

    add bass as a peer dependency

--- spec/IntegrationSpec.js
+++ spec/IntegrationSpec.js
@@ -2,8 +2,4 @@ const path = require('path');
 
 const AdapterIntegrationSpec = require('../node_modules/bass/spec/AdapterIntegrationSpec');
 
-describe('bass-nedb', AdapterIntegrationSpec('bass-nedb', {
-    adapters: [
-        path.resolve('')
-    ]
-}));
+describe('bass-nedb', AdapterIntegrationSpec('bass-nedb'));
