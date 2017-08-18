038e63151e8067999597d1575324e4c526385a3f
Marc Roulias <marc@marcroulias.com>
2017-08-18 01:06:25 -0700

    fixed connection creation so that it runs callback after all datastores are created

--- spec/IntegrationSpec.js
+++ spec/IntegrationSpec.js
@@ -1,5 +1,20 @@
+const fs = require('fs-extra');
 const path = require('path');
 
 const AdapterIntegration = require('../node_modules/bass/spec/AdapterIntegration');
 
-describe('bass-nedb', AdapterIntegration('bass-nedb'));
+const dbPath = path.join(__dirname, 'tmp');
+
+if (fs.existsSync(dbPath)) {
+    fs.removeSync(dbPath);
+}
+
+describe('bass-nedb', AdapterIntegration('bass-nedb', {
+    // connections: {
+    //     default: {
+    //         adapter: 'bass-nedb',
+    //         directory: dbPath
+    //     }
+    // }
+
+}));
